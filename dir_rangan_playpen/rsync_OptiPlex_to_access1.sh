3eeba19d4d45492ff4a0b9a8682d581821013055
adirangan <adirangan@yahoo.com>
2021-07-02 23:39:07 -0400

    searching for heterogeneity example

---
+++ dir_rangan_playpen/rsync_OptiPlex_to_access1.sh
@@ -0,0 +1,2 @@
+rsync -avum /home/rangan/dir_cryoem/dir_rangan_playpen/*.m /home/rangan/dir_cryoem/dir_rangan_playpen/*.sh /home/rangan/dir_cryoem/dir_rangan_playpen/*.tex /home/rangan/dir_cryoem/dir_rangan_playpen/*.txt rangan@access1.cims.nyu.edu:/data/rangan/dir_cryoem/dir_rangan_playpen/
+
