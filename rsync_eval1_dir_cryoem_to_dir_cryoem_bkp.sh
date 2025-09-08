2837968987ecf75c2d3519ab93387af688275c9e
adirangan <adirangan@yahoo.com>
2025-09-08 13:05:01 -0400

    dir_rangan_python

--- rsync_eval1_dir_cryoem_to_dir_cryoem_bkp.sh
+++ rsync_eval1_dir_cryoem_to_dir_cryoem_bkp.sh
@@ -1,16 +1,13 @@
 rsync -avum \
       ~/dir_cryoem/* \
       ~/dir_cryoem_bkp/ \
-      --exclude="EMPM/" \
-      --exclude="CryoLike/" \
       --include="*/" \
       --include="*.f" \
       --include="*.c" \
       --include="*.make" \
-      --include="*.m" \
       --include="*.py" \
+      --include="*.m" \
       --include="*.sh" \
-      --include="*.txt" \
       --include="*.tex" \
       --include="*.pdf" \
       --exclude="*" ;
